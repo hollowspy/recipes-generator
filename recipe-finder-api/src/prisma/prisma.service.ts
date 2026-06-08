a1d91dc8e1c2a0537b4453b9011457af57805813
julien.niedzwiecki <julien.niedzwiecki@abtasty.com>
2026-06-08 12:05:57 +0200

    change source for database url

--- recipe-finder-api/src/prisma/prisma.service.ts
+++ recipe-finder-api/src/prisma/prisma.service.ts
@@ -5,8 +5,12 @@ import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
+    const dbUrl = process.env.DATABASE_URL;
+    if (!dbUrl) {
+      throw new Error('DATABASE_URL must be defined or must be a string');
+    }
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString: dbUrl,
     });
     super({ adapter });
   }
